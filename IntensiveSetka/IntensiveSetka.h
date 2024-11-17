f28e282d4d260026563aa9c512a77ef246985af6
NASTYA_FZF <2002marev@gmail.com>
2024-11-17 18:04:12 +0300

    Добавьте файлы проекта.

---
+++ IntensiveSetka/IntensiveSetka.h
@@ -0,0 +1,32 @@
+﻿
+// IntensiveSetka.h: главный файл заголовка для приложения PROJECT_NAME
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "включить pch.h до включения этого файла в PCH"
+#endif
+
+#include "resource.h"		// основные символы
+
+
+// CIntensiveSetkaApp:
+// Сведения о реализации этого класса: IntensiveSetka.cpp
+//
+
+class CIntensiveSetkaApp : public CWinApp
+{
+public:
+	CIntensiveSetkaApp();
+
+// Переопределение
+public:
+	virtual BOOL InitInstance();
+
+// Реализация
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CIntensiveSetkaApp theApp;
---
+++ IntensiveSetka/IntensiveSetkaDlg.h
@@ -0,0 +1,33 @@
+﻿
+// IntensiveSetkaDlg.h: файл заголовка
+//
+
+#pragma once
+
+
+// Диалоговое окно CIntensiveSetkaDlg
+class CIntensiveSetkaDlg : public CDialogEx
+{
+// Создание
+public:
+	CIntensiveSetkaDlg(CWnd* pParent = nullptr);	// стандартный конструктор
+
+// Данные диалогового окна
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_INTENSIVESETKA_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// поддержка DDX/DDV
+
+
+// Реализация
+protected:
+	HICON m_hIcon;
+
+	// Созданные функции схемы сообщений
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
